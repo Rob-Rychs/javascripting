abd24df32515f850525d6e1387d5ed65dc462c82
Rob-Rychs <Robrychel@gmail.com>
2017-09-24 13:24:32 -0700

    even more .find

---
+++ ES6-Complete-Guide/array-helper-methods/get-account-by-balance-using-find.js
@@ -0,0 +1,11 @@
+// What's Your Balance?
+
+// Find the account with a balance of 12 and assign it to the variable 'account'.
+
+var accounts = [
+  { balance: -10 },
+  { balance: 12 },
+  { balance: 0 }
+];
+
+var account = accounts.find(account => account.balance === 12);
