fb320eb21a792c890214c7677133d69c304d2c75
Rob-Rychs <Robrychel@gmail.com>
2017-09-24 16:33:11 -0700

    added an arrowfunc example and moved gulpfiles to own folder

---
+++ ES6-Complete-Guide/arrow-functions/es5-func-to-es6-arrowfunc.js
@@ -0,0 +1,13 @@
+// es5
+const addEs5 = function(a, b) {
+  return a + b;
+}
+
+addEs5(1, 2);
+
+// es6
+const addEs6 = (a,b) => {
+  return a + b;
+}
+// or even
+const addEs6again = (a, b) => a + b;
