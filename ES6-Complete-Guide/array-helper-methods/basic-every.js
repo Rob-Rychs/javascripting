e2add05c29a165383e93817157f2638cae20bbd5
Rob-Rychs <Robrychel@gmail.com>
2017-09-24 14:29:35 -0700

    a basic example for .every and then .some

---
+++ ES6-Complete-Guide/array-helper-methods/basic-every.js
@@ -0,0 +1,13 @@
+// Finding Submitted Users
+
+// Given an array of users, return 'true' if every user has submitted a request form.  Assign the result to the variable 'hasSumbmitted'.
+
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user) {
+    return user.hasSubmitted === true;
+});
---
+++ ES6-Complete-Guide/array-helper-methods/basic-some.js
@@ -0,0 +1,13 @@
+// In Progress Network Requests
+
+// Given an array of network objects representing network requests, assign the boolean 'true' to the variable 'inProgress' if any network request has a 'status' of 'pending'.
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some(function(request) {
+    return request.status == 'pending';
+});
