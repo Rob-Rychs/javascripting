b30e5e2982ae03eada1b62d271ee1f744a277c9c
Rob-Rychs <Robrychel@gmail.com>
2017-09-25 01:41:17 -0700

    really cool commit! make sure to review! added props from server to component and some node code to accept cmnd-line args

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -10,8 +10,13 @@ require('babel/register')({
     ignore: false
 });
 
+var data = [
+  { title: 'Shopping', detail: process.argv[3] },
+  { title: 'Hair cut', detail: process.argv[4] }
+];
+
 app.use('/', function(req, res) {
-  res.render('index', '');
+  res.render('index', {data: data});
 });
 
 app.listen(app.get('port'), function() {});
