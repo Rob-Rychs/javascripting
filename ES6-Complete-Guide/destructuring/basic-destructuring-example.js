da492dd9bf59b0500e8d97c4464c99eb61f6bf2b
Rob-Rychs <Robrychel@gmail.com>
2017-09-26 14:35:31 -0700

    more destruction mwahahah

---
+++ ES6-Complete-Guide/destructuring/basic-destructuring-example.js
@@ -0,0 +1,24 @@
+// Destructuring in Practice
+
+// The snippet of code below duplicates references to 'profile' inside of the 'isEngineer' function.  Perhaps we can reduce the amount of code used for referencing the 'title' and 'department' properties.  Refactor this code to use destructuring.  Can you get the body of the 'isEngineer' function down to a single line?
+// es5
+const profile = {
+  title: 'Engineer',
+  department: 'Engineering'
+};
+
+function isEngineer(profile) {
+  var title = profile.title;
+  var department = profile.department;
+  return title === 'Engineer' && department === 'Engineering';
+}
+
+// es6
+const profile = {
+  title: 'Engineer',
+  department: 'Engineering'
+};
+
+function isEngineer({ title, department }) {
+  return title === 'Engineer' && department === 'Engineering';
+}
