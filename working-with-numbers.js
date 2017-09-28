e5af7111c3ed0399bf650b402cf66a697052a957
Rob-Rychs <Robrychel@gmail.com>
2017-09-27 22:51:49 -0700

    added working-with-numbers and working-with-strings

---
+++ working-with-numbers.js
@@ -0,0 +1,17 @@
+// WORKING WITH NUMBERS
+
+// +
+// -
+// *
+// /
+// %
+
+// digits are precise to 16 decimal points
+
+.toFixed() // rounds to x decimal places
+
+.Number("3.14") // converts string to number
+
+.parseInt("5") // converted string to integer
+
+.parseFloat("3.14") // converted string to float
---
+++ working-with-strings.js
@@ -0,0 +1,23 @@
+// WORKING WITH STIRNGS:
+
+.length vs .lengthOf() // get length of string
+
+.indexOf("") // get begining character of search string
+
+.slice(x,y) // slices a string at index start (x) to index end (y) (y=optional)
+
+.substr(x,y) // gets a substring by index start (x) and length of substring (y)
+
+.replace(x,y) // replaces string x with string y
+
+.charAt(x) // character at index x
+
+.split() // splits a string into an array, use .split(" ") to split the words between the spaces
+
+.trim() // trims white space of string
+
+.toUpperCase() 
+
+.toLowerCase() 
+
+strToStyle.property() .big() .bold() .fontcolor("blue") .fontsize("8em") .italics() .link("url") .small() .strike() .sub() .sup()
