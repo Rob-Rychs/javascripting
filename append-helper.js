7fb3ba0448f98351cbf8064063d0badfbfaad2fd
Rob-Rychs <Robrychel@gmail.com>
2017-10-03 18:22:30 -0700

    added func comments

--- append-helper.js
+++ append-helper.js
@@ -1,3 +1,5 @@
 function append(parent, el) {
-  return parent.appendChild(el); // Append the second parameter(element) to the first one
+  return parent.appendChild(el); 
 }
+// function append() accepts two arguments, the parent element and the element to append to the parent
+// Append the second parameter(el) to the first one
--- array-filtering.js
+++ array-filtering.js
@@ -1,5 +1,8 @@
+// array of numbers 1-10
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var filtered = numbers.filter(function evenNumbers (number) {
 	return number % 2 === 0;
 });
-console.log(filtered);
+// function filtered() returns the numbers array filtered by the function evenNumbers() with .filter()
+// function evenNumbers() accepts a number as an argument and  returns it if it's divisible by two
+console.log(filtered); // logs [2, 4, 6, 8, 10]
