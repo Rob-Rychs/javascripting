317464f860752e2cd2298dfb122dc1c4a6c62977
Rob-Rychs <Robrychel@gmail.com>
2017-10-18 10:44:11 -0700

    added a quick-demo server in node-express/

--- node-express/hello-world-express.js
+++ node-express/hello-world-express.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const port = 3300;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+}) // run `nc localhost 3300` in terminal to get response
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Server running @ localhost:${port}`)
 })
+// Almost-equivalent code with regular Node:
+// response.write('Hello, world!');
+// response.end();
