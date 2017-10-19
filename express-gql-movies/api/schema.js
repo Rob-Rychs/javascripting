2040f508e6fb08223fc2c27b3f85c425550e011e
Rob-Rychs <Robrychel@gmail.com>
2017-10-19 11:01:13 -0700

    added movies gql schema in typeDefs

--- express-gql-movies/api/schema.js
+++ express-gql-movies/api/schema.js
@@ -1,7 +1,27 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './resolvers'; // Next step!
 const typeDefs = `
-  # Your schema goes here...
+  type Person {
+    id: ID!
+    name: String!
+    birthday: String
+    placeOfBirth: String
+    bio: String
+    filmography: [Movie]
+  }
+  type Movie {
+    id: ID!
+    title: String!
+    releaseDate: String
+    stars: [Person]
+    director: Person
+  }
+  type Query {
+    people: [Person]
+    person(id: ID!): Person
+    movies: [Movie]
+    movie(id: ID!): Movie
+  }
 `;
 export default makeExecutableSchema({
   typeDefs,
