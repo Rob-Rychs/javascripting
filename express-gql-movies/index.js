601b47a6ea3b5ea8b8bf64ee493d2f2b19610ea9
Rob-Rychs <Robrychel@gmail.com>
2017-10-19 10:15:20 -0700

    👉💨 import graphqlExpress into index.js

--- express-gql-movies/index.js
+++ express-gql-movies/index.js
@@ -1,3 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
+
+import { 
+  graphqlExpress, 
+  graphiqlExpress 
+} from 'apollo-server-express';
+import schema from './api/schema'; // Next step!
+const GQL_PORT = process.env.PORT; // Where does this come from?
+// Where we will send all of our GraphQL requests
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+// A route for accessing the GraphiQL tool
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+app.listen(GQL_PORT, () => console.log(
+  `GraphQL is now running on http://localhost:${GQL_PORT}/graphql`
+));
