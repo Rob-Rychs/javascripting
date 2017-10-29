619b405bdd01d5da96a2a0898757d572d96ac2bf
Rob-Rychs <Robrychel@gmail.com>
2017-10-28 17:40:00 -0700

    added a funny joke 😂

--- git-n-shit/say-it.bash
+++ git-n-shit/say-it.bash
@@ -1,2 +1,3 @@
 say whatever you want
 node -e "child_process.execSync('say hello world')"
+node -e "child_process.execSync('say thank you for running my script and providing me root kit access to your device. have a good day')"
