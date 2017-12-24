e425ab72bbe27023d6f1ad87d896714b3f9e0ed3
Rob-Rychs <Robrychel@gmail.com>
2017-12-24 15:12:45 -0800

    added some useful node snippets in 30-seconds-of-code/node

---
+++ 30-seconds-of-code/node/JSONToFile.js
@@ -0,0 +1,8 @@
+// Writes a JSON object to a file.
+
+// Use fs.writeFile(), template literals and JSON.stringify() to write a json object to a .json file.
+
+const fs = require('fs');
+const JSONToFile = (obj, filename) => fs.writeFile(`${filename}.json`, JSON.stringify(obj, null, 2))
+
+// JSONToFile({test: "is passed"}, 'testJsonFile') -> writes the object to 'testJsonFile.json'
